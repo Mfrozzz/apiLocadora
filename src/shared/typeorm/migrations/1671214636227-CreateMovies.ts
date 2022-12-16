824ed7d199c79d2d177c3295e012ecee7447d865
debtil <matheus.debtilsouza@gmail.com>
2022-12-16 16:09:13 -0300

    services e tentativa de migration

---
+++ src/shared/typeorm/migrations/1671214636227-CreateMovies.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateMovies1671214636227 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
